3ab0146daba3e4ca7737566cdd9ea3a7edf5db89
fire <gioihannaochochungta9x@gmail.com>
2020-07-03 11:12:34 +0700

    tao controller and database house

---
+++ database/migrations/2020_07_03_040659_create_table_house.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableHouse extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('houses', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('house_category');
+            $table->string('room_category');
+            $table->string('bedroom_amount');
+            $table->string('bathroom_amount');
+            $table->longText('description');
+            $table->string('price');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_house');
+    }
+}
