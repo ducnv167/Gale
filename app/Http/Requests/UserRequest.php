776cfb721f55f0b4e4a8b46e9d968556b6a43d5c
TranTam-C0220H2 <tam.tvt.ptit@gmail.com>
2020-07-07 16:22:22 +0700

    up ajax to validate register

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -24,13 +24,13 @@ class UserRequest extends FormRequest
     public function rules()
     {
         return [
-          'name' => 'required|min:3',
-          'email' => 'required|email',
-          'password' => 'required|min:6|max:20',
-          'repeatPassword' => 'required|min:6|max:20|same:password',
-          'phone' => 'required|min:10|numeric',
-          'role' => 'required|min:5|max:6',
-          'image' => 'required|image'
+            'name' => 'required|min:3',
+            'email' => 'required|email',
+            'password' => 'required|min:6|max:20',
+            'repeatPassword' => 'required|min:6|max:20|same:password',
+            'phone' => 'required|min:10|numeric',
+            'role' => 'required|min:5|max:6',
+            'image' => 'required|image'
         ];
     }
 }
