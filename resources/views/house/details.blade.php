ccbd7f80c7a66e52a7e017700e4042e5dd7f38e6
Nam <nguyentrongnama8@gmail.com>
2020-07-07 17:49:17 +0700

    sua view how detail

--- resources/views/house/details.blade.php
+++ resources/views/house/details.blade.php
@@ -1,6 +1,20 @@
 @extends('master')
 @section('content')
-
+<section class="hero-wrap hero-wrap-2" style="background-image: url('{{ asset('images/bg_1.jpg') }}');"
+    data-stellar-background-ratio="0.5">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-center">
+            <div class="col-md-9 ftco-animate pb-0 text-center">
+                <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i
+                                class="fa fa-chevron-right"></i></a></span> <span class="mr-2"><a
+                            href="properties.html">Properties <i class="fa fa-chevron-right"></i></a></span>
+                    <span>Properties Single <i class="fa fa-chevron-right"></i></span></p>
+                <h1 class="mb-3 bread">Property Details</h1>
+            </div>
+        </div>
+    </div>
+</section>
 <section class="ftco-section ftco-property-details">
     <div class="container">
         <div class="row justify-content-center">
@@ -95,9 +109,9 @@
                                         <li class="check"><span class="fa fa-check-circle"></span>Garage: 2</li>
                                     </ul>
                                 </div>
-                                <div class="col-md-4">
+                                {{-- <div class="col-md-4">
                                     <a class="btn btn-info" href="">Rent Now!</a>
-                                </div>
+                                </div> --}}
                             </div>
                         </div>
                     </div>
@@ -105,7 +119,7 @@
                     <div class="tab-pane fade" id="pills-manufacturer" role="tabpanel"
                         aria-labelledby="pills-manufacturer-tab">{!!$house->description!!}
                     </div>
-                    <div class="tab-pane fade" id="pills-review" role="tabpanel" aria-labelledby="pills-review-tab">
+                    {{-- <div class="tab-pane fade" id="pills-review" role="tabpanel" aria-labelledby="pills-review-tab">
                         <div class="row">
                             <div class="col-md-7">
                                 <h3 class="head">Reviews</h3>
@@ -176,7 +190,7 @@
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </div> --}}
                 </div>
             </div>
         </div>
