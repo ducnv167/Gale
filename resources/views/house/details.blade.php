67eda173f2d82c3ffa220c6d0347c0e2c5ccb589
Nam <nguyentrongnama8@gmail.com>
2020-07-14 08:00:25 +0700

    tach js ra file rieng

--- resources/views/house/details.blade.php
+++ resources/views/house/details.blade.php
@@ -20,7 +20,7 @@
 </section>
 <section class="ftco-section ftco-property-details">
     <div class="container">
-        <div class="row justify-content-center">
+        <div class="row">
             <div class="col-md-8">
                 <div class="property-details">
                     <div id="carouselExampleIndicators" class="carousel slide container" data-ride="carousel">
@@ -341,97 +341,4 @@
         </div>
     </div>
 </section>
-<script>
-    $(document).ready(function () {
-            let average = $('.average').text();
-            if (average < 1) {
-                $('.star-1, .star-2, .star-3, .star-4, .star-5').css('color', 'gray');
-            } else if (average < 2) {
-                $('.star-2, .star-3, .star-4, .star-5').css('color', 'gray');
-            } else if (average < 3) {
-                $('.star-3, .star-4, .star-5').css('color', 'gray');
-            } else if (average < 4) {
-                $('.star-4, .star-5').css('color', 'gray');
-            } else if (average < 5) {
-                $('.star-5').css('color', 'gray');
-            }
-            let linkMap = $('.map').text();
-            $('.google-map').html(linkMap);
-        })
-</script>
-<script>
-    $(document).ready(function (date) {
-            var bookedDays = [];
-            function getBookedDayOfHouse() {
-                const houseId = $('#name-house').attr("data-id");
-                let origin = window.location.origin;
-                $.ajax({
-                    type: "GET",
-                    url: origin + "/houses/" + houseId + '/booked-day',
-                    dataType: "json",
-                    success: function (response) {
-                        console.log($('#endDate').val())
-                        bookedDays = response;
-                        var today = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate());
-                        $('#startDate').datepicker({
-                            uiLibrary: 'bootstrap4',
-                            iconsLibrary: 'fontawesome',
-                            format: 'dd/mm/yyyy',
-                            minDate: function () {
-                                if ($('#endDate').val() === '') {
-                                   return today;
-                                } else {
-                                    let
-                                     newEndDateFormat = $('#endDate').val().split("/");
-                                    let endDate = new Date(newEndDateFormat[2], newEndDateFormat[1] - 1, newEndDateFormat[0]);
-                                    for (let i = bookedDays.length - 1; i >= 0; i--) {
-                                        if (endDate > new Date(bookedDays[i].split("/")[2], bookedDays[i].split("/")[1] - 1, bookedDays[i].split("/")[0])) {
-                                            return bookedDays[i];
-                                        }
-                                     }
-                                }
-                            },
-                            maxDate: function () {
-                                let newMaxDateFormat = $('#endDate').val().split("/");
-                                let maxDate = new Date(newMaxDateFormat[2], newMaxDateFormat[1] - 1, newMaxDateFormat[0]);
-                                return maxDate.setDate(maxDate.getDate() - 1);
-                            },
-                            disableDates: response,
-                            showOtherMonths: false,
-                        });
-                        disableEndTime(response)
-                    }
-                });
-            }
-            function disableEndTime(day) {
-                let today = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate());
-                $('#endDate').datepicker({
-                    uiLibrary: 'bootstrap4',
-                    iconsLibrary: 'fontawesome',
-                    minDate: function () {
-                        if ($('#startDate').val() === '') {
-                            return today;
-                        } else {
-                        let newMinDateFormat = $('#startDate').val().split("/");
-                        let minDate = new Date(newMinDateFormat[2], newMinDateFormat[1] - 1, newMinDateFormat[0]);
-                        return minDate.setDate(minDate.getDate() + 1);
-                        }
-                    },
-                    maxDate: function () {
-                        let newStartDateFormat = $('#startDate').val().split("/");
-                        let startDate = new Date(newStartDateFormat[2], newStartDateFormat[1] - 1, newStartDateFormat[0]);
-                        for (let i = 0; i < bookedDays.length; i++) {
-                            if (startDate < new Date(bookedDays[i].split("/")[2], bookedDays[i].split("/")[1] - 1, bookedDays[i].split("/")[0])) {
-                                return bookedDays[i];
-                            }
-                        }
-                    },
-                    showOtherMonths: false,
-                    format: 'dd/mm/yyyy',
-                    disableDates: day,
-                });
-            }
-            getBookedDayOfHouse();
-        });
-</script>
 @endsection
